47968ffb312654a143d3756c44ed5176a84a0c1e
JOONO <aspdosa@gmail.com>
2020-05-01 13:16:25 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://aspdosa:aspdosa@boiler-plate-ch4up.mongodb.net/test?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected...."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!~~안녕하세요 ~ "));
+app.get("/", (req, res) =>
+  res.send("Hello World!~~안녕하세요! 코로나 잘가!!!")
+);
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
